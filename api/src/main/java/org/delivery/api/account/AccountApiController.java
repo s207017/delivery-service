ff3a0b6b24fd3af2e972bc4b33dd4c2f98393c00
Choi Seunghwan <choish03030@gmail.com>
2025-08-24 14:34:27 +0800

    feat: Modified the save() function to me() function
    me() function just returns a sample user for testing purposes

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,12 +1,15 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -15,9 +18,12 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+        return AccountMeResponse.builder()
+                .name("Choi Seunghwan")
+                .email("choi@gmail.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
--- api/src/main/java/org/delivery/api/account/model/AccountMeResponse.java
+++ api/src/main/java/org/delivery/api/account/model/AccountMeResponse.java
@@ -1,4 +1,17 @@
 package org.delivery.api.account.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AccountMeResponse {
+    private String email;
+    private String name;
+    private LocalDateTime registeredAt;
 }
