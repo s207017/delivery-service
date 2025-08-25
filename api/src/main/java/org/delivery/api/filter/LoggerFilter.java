ca6b93cba001c7a168f4e8b6e867edb9d554f9ca
Choi Seunghwan <choish03030@gmail.com>
2025-08-25 21:26:42 +0800

    feat: Implemented LoggerFilter
    Implemented LoggerFilter that reads header values of response and requests and log them

---
+++ api/src/main/java/org/delivery/api/filter/LoggerFilter.java
@@ -0,0 +1,53 @@
+package org.delivery.api.filter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@Component
+@Slf4j
+public class LoggerFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var req =  new ContentCachingRequestWrapper( (HttpServletRequest) servletRequest);
+        var res = new ContentCachingResponseWrapper( (HttpServletResponse) servletResponse);
+
+        filterChain.doFilter(req, res); //this marks the point where filter runs
+
+        //request information
+        var headerNames = req.getHeaderNames();
+        var headerValues = new StringBuilder();
+
+        headerNames.asIterator().forEachRemaining(headerKey -> {
+            var headerValue = req.getHeader(headerKey);
+
+            // authorization-token : ???, user-agent : ???
+            headerValues.append(headerKey).append(" : ").append(" , ");
+        });
+
+        var requestBody = new String(req.getContentAsByteArray());
+        log.info(">>>>> header : {}, body : {}", headerValues, requestBody);
+
+        //response information
+        var responseHeaderValues = new StringBuilder();
+        res.getHeaderNames().forEach(headerKey -> {
+            var headerValue = res.getHeader(headerKey);
+
+            responseHeaderValues.append(headerKey).append(" : ").append(" , ");
+        });
+
+        var responseBody = new String(res.getContentAsByteArray());
+        log.info("<<<<< header: {}, body: {}", responseHeaderValues, responseBody);
+
+        //without the copyBodyToResponse(), the response leaving filter will be empty
+        //since we read it once above
+        res.copyBodyToResponse();
+    }
+}
